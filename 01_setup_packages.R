0cdf66cd14fafb34371e57f0446b3624ebe5bfea
Kriti Patra <kritipatra25@iisertvm.ac.in>
2026-05-19 17:43:21 +0530

    01_setup_packages.R

---
+++ 01_setup_packages.R
@@ -0,0 +1,23 @@
+if (!require("BiocManager"))
+    install.packages("BiocManager")
+
+BiocManager::install(c(
+    "SBGNview",
+    "org.At.tair.db",
+    "AnnotationDbi"
+))
+
+install.packages(c(
+    "xml2",
+    "rsvg",
+    "RColorBrewer",
+    "tidyverse"
+))
+
+library(SBGNview)
+library(tidyverse)
+library(org.At.tair.db)
+library(AnnotationDbi)
+library(xml2)
+library(rsvg)
+library(RColorBrewer)
