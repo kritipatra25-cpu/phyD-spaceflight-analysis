47833f04a746a07ce48a872a83ff478b2285c349
Kriti Patra <kritipatra25@iisertvm.ac.in>
2026-06-06 11:13:06 +0530

    Update GCR ABA_heatmap.R

--- heatmap_OSD658/Scripts/GCR ABA_heatmap.R
+++ heatmap_OSD658/Scripts/GCR ABA_heatmap.R
@@ -1,68 +1,52 @@
 library(readr)
 
-file_path <- file.choose()
+# Select OSD-658 file manually
+expr <- read.csv(file.choose())
 
-expr <- read.csv(
-  file_path,
-  check.names = FALSE
-)
-
+# Correct ABA genes
 aba_genes <- c(
-  "AT4G26080", # NCED3
-  "AT3G14440", # NCED5
-  "AT3G24220", # NCED9
-  "AT1G52340", # PYL5
-  "AT5G05440", # PYL8
-  "AT4G26080", # ABI1
-  "AT5G57050", # ABI2
-  "AT3G11410", # ABI5
-  "AT3G50500", # SnRK2.3
-  "AT4G33950"  # SnRK2.2
+  "AT3G14440",  # NCED3
+  "AT1G30100",  # NCED5
+  "AT4G26080",  # ABI1
+  "AT5G57050",  # ABI2
+  "AT5G05440",  # PYL5
+  "AT5G53160",  # PYL8
+  "AT3G50500",  # SnRK2.2
+  "AT5G66880",  # SnRK2.3
+  "AT2G36270"   # ABI5
 )
 
 aba_labels <- c(
-  "NCED3",
-  "NCED5",
-  "NCED9",
-  "PYL5",
-  "PYL8",
-  "ABI1",
-  "ABI2",
-  "ABI5",
-  "SnRK2.3",
-  "SnRK2.2"
+  "NCED3 (AT3G14440)",
+  "NCED5 (AT1G30100)",
+  "ABI1 (AT4G26080)",
+  "ABI2 (AT5G57050)",
+  "PYL5 (AT5G05440)",
+  "PYL8 (AT5G53160)",
+  "SnRK2.2 (AT3G50500)",
+  "SnRK2.3 (AT5G66880)",
+  "ABI5 (AT2G36270)"
 )
 
 aba_matrix <- expr[
   match(aba_genes, expr$TAIR),
   c(
-    "TAIR",
     "Log2fc_(mixed radiation & 40 centigray)v(non-irradiated & nan Not Applicable)",
     "Log2fc_(mixed radiation & 80 centigray)v(non-irradiated & nan Not Applicable)"
   )
 ]
 
-aba_matrix$Label <- paste0(
-  aba_labels,
-  " (",
-  aba_matrix$TAIR,
-  ")"
-)
-
-aba_export <- aba_matrix[, c(
-  "Label",
-  "Log2fc_(mixed radiation & 40 centigray)v(non-irradiated & nan Not Applicable)",
-  "Log2fc_(mixed radiation & 80 centigray)v(non-irradiated & nan Not Applicable)"
-)]
-
-colnames(aba_export) <- c(
-  "Label",
-  "GCR_40cGy",
-  "GCR_80cGy"
+aba_export <- data.frame(
+  id = aba_labels,
+  GCR_40cGy = aba_matrix[,1],
+  GCR_80cGy = aba_matrix[,2]
 )
 
 write.csv(
   aba_export,
   "GCR_ABA_Morpheus.csv",
-  row.names = FALSE
+  row.names = FALSE,
+  quote = FALSE
 )
+
+print(aba_export)
