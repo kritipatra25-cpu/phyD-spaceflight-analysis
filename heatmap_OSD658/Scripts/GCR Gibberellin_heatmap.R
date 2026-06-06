c029d4a2fa21c8161451335e94a533a005f80d56
Kriti Patra <kritipatra25@iisertvm.ac.in>
2026-06-06 11:26:40 +0530

    Update GCR Gibberellin_heatmap.R

--- heatmap_OSD658/Scripts/GCR Gibberellin_heatmap.R
+++ heatmap_OSD658/Scripts/GCR Gibberellin_heatmap.R
@@ -1,5 +1,6 @@
 library(readr)
 
+# Select OSD-658 file manually
 file_path <- file.choose()
 
 expr <- read.csv(
@@ -8,15 +9,35 @@ expr <- read.csv(
 )
 
 ga_genes <- c(
-  "AT1G15550", # GA3OX1
-  "AT5G07200", # GA20OX3
-  "AT1G60980"  # GA20OX4
+  "AT4G25420",  # GA20OX1
+  "AT5G51810",  # GA20OX2
+  "AT5G07200",  # GA20OX3
+  "AT1G15550",  # GA3OX1
+
+  "AT3G05120",  # GID1A
+  "AT3G63010",  # GID1B
+  "AT5G27320",  # GID1C
+  "AT4G24210",  # SLY1
+
+  "AT3G03450",  # RGL2
+  "AT1G14920",  # GAI
+  "AT2G01570"   # RGA
 )
 
 ga_labels <- c(
-  "GA3OX1",
-  "GA20OX3",
-  "GA20OX4"
+  "GA20OX1 (AT4G25420)",
+  "GA20OX2 (AT5G51810)",
+  "GA20OX3 (AT5G07200)",
+  "GA3OX1 (AT1G15550)",
+
+  "GID1A (AT3G05120)",
+  "GID1B (AT3G63010)",
+  "GID1C (AT5G27320)",
+  "SLY1 (AT4G24210)",
+
+  "RGL2 (AT3G03450)",
+  "GAI (AT1G14920)",
+  "RGA (AT2G01570)"
 )
 
 ga_matrix <- expr[
@@ -28,27 +49,27 @@ ga_matrix <- expr[
   )
 ]
 
-ga_matrix$Label <- paste0(
-  ga_labels,
-  " (",
-  ga_matrix$TAIR,
-  ")"
-)
-
-ga_export <- ga_matrix[, c(
-  "Label",
-  "Log2fc_(mixed radiation & 40 centigray)v(non-irradiated & nan Not Applicable)",
-  "Log2fc_(mixed radiation & 80 centigray)v(non-irradiated & nan Not Applicable)"
-)]
-
-colnames(ga_export) <- c(
-  "Label",
+colnames(ga_matrix) <- c(
+  "TAIR",
   "GCR_40cGy",
   "GCR_80cGy"
 )
+
+ga_matrix$id <- ga_labels
+
+ga_export <- ga_matrix[
+  ,
+  c(
+    "id",
+    "GCR_40cGy",
+    "GCR_80cGy"
+  )
+]
 
 write.csv(
   ga_export,
   "GCR_Gibberellin_Morpheus.csv",
   row.names = FALSE
 )
+
+print(ga_export)
