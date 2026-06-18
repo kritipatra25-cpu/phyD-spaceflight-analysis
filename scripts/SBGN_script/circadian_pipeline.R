ec36b82115b9dcbc497da02c705a4596e41ad23d
Kriti Patra <kritipatra25@iisertvm.ac.in>
2026-06-18 21:32:46 +0530

    Update circadian_pipeline.R

--- scripts/SBGN_script/circadian_pipeline.R
+++ scripts/SBGN_script/circadian_pipeline.R
@@ -1,208 +1,209 @@
-# =========================================================
-# CIRCADIAN RHYTHM SBGNview PIPELINE
-# =========================================================
-
 library(SBGNview)
 library(xml2)
 library(rsvg)
 
-source("03_shared_functions.R")
+#################################################
+# COLOR FUNCTION
+#################################################
 
-SBGNview(
-    
-    input.sbgn =
-        "http___identifiers.org_reactome_R-HSA-400253.sbgn",
-    
-    output.file =
-        "Circadian_Base"
-)
+fc_to_color <- function(fc){
 
-svg <- read_xml(
-    "Circadian_Base_http___identifiers.org_reactome_R-HSA-400253.sbgn.svg"
-)
-
-texts <- xml_find_all(
-    
-    svg,
-    
-    ".//*[local-name()='text']"
-)
-
-labels <- trimws(
-    
-    xml_text(texts)
-)
-
-labels <- labels[
-    
-    labels != ""
-]
-
-
-grep(
-    
-    "CLOCK|BMAL|PER|CRY|circadian|casein|REV|ROR|NPAS",
-    
-    labels,
-    
-    ignore.case = TRUE,
-    
-    value = TRUE
-)
-
-
-circadian_fc <- data.frame(
-    
-    node = c(
-        
-        "CLOCK",
-        "BMAL1",
-        "NPAS2",
-        "CRY1",
-        "CRY2",
-        "PER1",
-        "PER2",
-        "RORA"
-    ),
-    
-    fold_change = c(
-        
-        2.8,
-        3.5,
-        2.2,
-        -2.1,
-        -1.7,
-        -3.0,
-        -2.5,
-        1.9
-    )
-)
-
-for(i in 1:nrow(circadian_fc)){
-    
-    current_node <- circadian_fc$node[i]
-    
-    current_fc <- circadian_fc$fold_change[i]
-    
-    current_color <- fc_to_color(current_fc)
-    
-    matched_nodes <- xml_find_all(
-        
-        svg,
-        
-        paste0(
-            
-            ".//*[local-name()='text' and contains(., '",
-            
-            current_node,
-            
-            "')]"
-        )
-    )
-    
-    for(j in matched_nodes){
-        
-        xml_set_attr(
-            j,
-            "fill",
-            current_color
-        )
-        
-        xml_set_attr(
-            j,
-            "font-size",
-            "20"
-        )
-        
-        xml_set_attr(
-            j,
-            "font-weight",
-            "bold"
-        )
-    }
+  if(fc >= 4){
+    "#8B0000"
+  } else if(fc >= 2){
+    "#FF4500"
+  } else if(fc >= 1){
+    "#FFA07A"
+  } else if(fc <= -1.5){
+    "#00008B"
+  } else if(fc <= -0.5){
+    "#4169E1"
+  } else if(fc < 0){
+    "#87CEFA"
+  } else {
+    "black"
+  }
 }
 
+#################################################
+# RENDER ORIGINAL SBGN
+#################################################
+
+SBGNview(
+
+  input.sbgn =
+    "http___identifiers.org_panther.pathway_P00015.sbgn",
+
+  output.file =
+    "OSD120_Circadian"
+
+)
+
+#################################################
+# OPEN SVG
+#################################################
+
+svg <- read_xml(
+  "OSD120_Circadian_http___identifiers.org_panther.pathway_P00015.sbgn.svg"
+)
+
+#################################################
+# OSD120 VALUES
+#################################################
+
+circadian_fc <- data.frame(
+
+  node = c(
+
+    "Cry",
+    "Per",
+    "Clock",
+    "BMAL1",
+    "bmal1",
+    "Rev-erb",
+    "rev-erb"
+
+  ),
+
+  fold_change = c(
+
+    mean(c(-0.142,-0.487)),             # CRY1 + CRY2
+
+    mean(c(-1.80,-0.97,-1.11,-0.44)),  # PRR5 PRR7 PRR3 TOC1
+
+    mean(c(5.76,4.53)),                # LHY + CCA1
+
+    mean(c(5.76,4.53)),
+    mean(c(5.76,4.53)),
+
+    -1.82,                             # GI
+    -1.82
+
+  )
+)
+
+#################################################
+# COLOR NODES
+#################################################
+
+for(i in 1:nrow(circadian_fc)){
+
+  current_node <- circadian_fc$node[i]
+
+  current_fc <- circadian_fc$fold_change[i]
+
+  current_color <- fc_to_color(current_fc)
+
+  matched_nodes <- xml_find_all(
+
+    svg,
+
+    paste0(
+      ".//*[local-name()='text' and normalize-space(text())='",
+      current_node,
+      "']"
+    )
+  )
+
+  cat(
+    current_node,
+    " -> ",
+    length(matched_nodes),
+    " matches\n"
+  )
+
+  for(j in matched_nodes){
+
+    xml_set_attr(
+      j,
+      "fill",
+      current_color
+    )
+
+    xml_set_attr(
+      j,
+      "font-size",
+      "24"
+    )
+
+    xml_set_attr(
+      j,
+      "font-weight",
+      "bold"
+    )
+  }
+}
+
+#################################################
+# EMPHASIZE KEY NODES
+#################################################
 
 master_nodes <- c(
-    
-    "CLOCK",
-    "BMAL1",
-    "PER1",
-    "PER2",
-    "CRY1",
-    "CRY2",
-    "NPAS2"
+
+  "Cry",
+  "Per",
+  "Clock",
+  "BMAL1"
+
 )
 
 for(lbl in master_nodes){
-    
-    nodes <- xml_find_all(
-        
-        svg,
-        
-        paste0(
-            
-            ".//*[local-name()='text' and contains(., '",
-            
-            lbl,
-            
-            "')]"
-        )
-    )
-    
-    for(n in nodes){
-        
-        xml_set_attr(
-            n,
-            "font-size",
-            "30"
-        )
-        
-        xml_set_attr(
-            n,
-            "font-weight",
-            "bold"
-        )
-    }
-}
 
+  nodes <- xml_find_all(
 
-rects <- xml_find_all(
-    
     svg,
-    
-    ".//*[local-name()='rect']"
-)
 
-if(length(rects) > 0){
-    
-    xml_set_attr(
-        
-        rects[[1]],
-        
-        "fill",
-        
-        "white"
+    paste0(
+      ".//*[local-name()='text' and normalize-space(text())='",
+      lbl,
+      "']"
     )
+  )
+
+  for(n in nodes){
+
+    xml_set_attr(
+      n,
+      "font-size",
+      "34"
+    )
+
+    xml_set_attr(
+      n,
+      "font-weight",
+      "bold"
+    )
+  }
 }
+
+#################################################
+# SAVE SVG
+#################################################
 
 write_xml(
-    
-    svg,
-    
-    "Circadian_Advanced.svg"
+
+  svg,
+
+  "OSD120_Circadian_Colored.svg"
+
 )
 
+#################################################
+# CONVERT TO PNG
+#################################################
 
 rsvg_png(
-    
-    "Circadian_Advanced.svg",
-    
-    "Circadian_Advanced.png",
-    
-    width = 5500,
-    
-    height = 4500
+
+  "OSD120_Circadian_Colored.svg",
+
+  "OSD120_Circadian_Colored.png",
+
+  width = 5000,
+
+  height = 4000
+
 )
 
-cat("\nCircadian rhythm pathway rendered successfully.\n")
+cat(
+  "\nOSD120 Circadian pathway rendered successfully.\n"
+)
